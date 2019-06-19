8a95383ef1965d377c2e8569f2949c216d62bcf2
DeveloperKANG <qoo1476@hanmail.net>
2019-06-19 11:42:29 +0900

    first commit

---
+++ src/main/java/com/dewith/demo_project/web/WebRestController.java
@@ -0,0 +1,14 @@
+package com.dewith.demo_project.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "HelloWorld";
+    }
+
+}
